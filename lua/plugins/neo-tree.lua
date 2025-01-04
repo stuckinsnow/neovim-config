a83ea2dc377bed5b160b6441c36ec461c005de6f
Philip <stuckinsnow@users.noreply.github.com>
2025-01-04 17:49:39 +0000

    change tree icons.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,6 +4,24 @@ return {
     default_component_configs = {
       indent = {
         with_markers = false,
+        -- padding = 0,
+        -- expander_collapsed = "",
+        -- expander_expanded = "",
+        expander_collapsed = "",
+        expander_expanded = "",
+      },
+      icon = {
+        -- folder_closed = "", -- default
+        -- folder_open = "", -- default
+        -- folder_closed = "",
+        -- folder_open = "",
+        folder_closed = "→",
+        folder_open = "↓",
+        folder_empty = "󰜌",
+      },
+      expanders = {
+        arrow_closed = "", -- Set to empty string to hide closed arrow
+        arrow_open = "", -- Set to empty string to hide open arrow
       },
     },
     filesystem = {
