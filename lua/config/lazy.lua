9d89f6720f7315f80e8fbeb4855a1b7c6ab9df2d
Philip <stuckinsnow@users.noreply.github.com>
2025-01-20 03:30:19 +0000

    update comment

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -53,7 +53,7 @@ require("lazy").setup({
       "gbprod/yanky.nvim",
       opts = {
         system_clipboard = {
-          sync_with_ring = false, -- Disable syncing with the system clipboard
+          sync_with_ring = false,
         },
       },
     },
