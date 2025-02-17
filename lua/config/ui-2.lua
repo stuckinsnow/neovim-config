58ac720aaf38e41baf0d502df88f7156250b7262
Philip <126236947+stuckinsnow@users.noreply.github.com>
2025-02-17 12:51:42 +0000

    Update ui-2.lua

--- lua/config/ui-2.lua
+++ lua/config/ui-2.lua
@@ -1,13 +1,17 @@
 -- Enable the custom StatusColumn for all buffers except Neo-tree
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "neo-tree",
+  pattern = "minifiles",
   callback = function() end,
 })
 
 -- Handle buffer-specific settings on entering a buffer
 vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
   callback = function()
-    if vim.bo.filetype ~= "neo-tree" and vim.bo.filetype ~= "copilot-chat" then
+    if vim.bo.filetype == "minifiles" then
+      vim.opt_local.relativenumber = true
+      vim.opt_local.number = false
+    end
+    if vim.bo.filetype ~= "neo-tree" and vim.bo.filetype ~= "minifiles" and vim.bo.filetype ~= "copilot-chat" then
       vim.opt_local.relativenumber = true
       vim.opt_local.number = true
     end
@@ -17,7 +21,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
 -- Adjust settings on leaving a window
 vim.api.nvim_create_autocmd("WinLeave", {
   callback = function()
-    if vim.bo.filetype ~= "neo-tree" and vim.bo.filetype ~= "copilot-chat" then
+    if vim.bo.filetype ~= "neo-tree" and vim.bo.filetype ~= "minifiles" and vim.bo.filetype ~= "copilot-chat" then
       vim.opt_local.relativenumber = false
       vim.opt_local.number = true
     end
